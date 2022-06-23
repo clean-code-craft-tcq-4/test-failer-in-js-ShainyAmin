7b477ba07828b981387a4175d39bf33f5a52b8f0
ShainyAmin <38464281+ShainyAmin@users.noreply.github.com>
2022-06-23 17:06:53 +0530

    Add files via upload

--- misaligned.js
+++ misaligned.js
@@ -1,16 +1,43 @@
 const {expect} = require('chai')
+const {ActualColorCodePairs} = require('./actualColorcodePair')
+var colorCodePairList = [];
+
+var majorColors = ["White ", "Red ", "Black ", "Yellow", "Violet"];
+var minorColors = ["Blue ", "Orange", "Green ", "Brown ", "Slate "];
 
 function print_color_map() {
-    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
-    for (let i = 0; i < majorColors.length; i++) {
-        for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
-        }
-    }
-    return majorColors.length * minorColors.length;
-}
+  for (let i = 0; i < majorColors.length; i++) {
+     for (let j=0; j < minorColors.length; j++) { 
+         colorCodePairList.push(`${i* 5 + j} | ${majorColors[i]} | ${minorColors[j]}`); }
+         } 
+         return majorColors.length * minorColors.length;
+ } 
+ 
+ function printColorCodeMisalignementTest() { 
+     var longestColorCodePairValue=colorCodePairList.reduce( function
+    (previousColorCodePairValue, currentColorCodePairValue) { 
+        return previousColorCodePairValue.length> currentColorCodePairValue.length ? previousColorCodePairValue : currentColorCodePairValue;
 
-result = print_color_map();
-expect(result).equals(25);
-console.log('All is well (maybe!)');
+    }
+
+    );
+    colorCodePairList.forEach(eachColourCodePairValue => {
+    expect(eachColourCodePairValue.length).equals(longestColorCodePairValue.length)
+    })
+
+    }
+
+    function falseColorPairTest() {
+    var comparedColorCodePairResult = colorCodePairList.length && ActualColorCodePairs.length &&
+    colorCodePairList.every(function(colourPair, index){
+    return colourPair === ActualColorCodePairs[index]
+    })
+
+    expect(comparedColorCodePairResult).equals(true);
+    }
+
+    result = print_color_map();
+    expect(result).equals(25);
+    printColorCodeMisalignementTest();
+    console.log('All is well (maybe!)');
+    falseColorPairTest();
